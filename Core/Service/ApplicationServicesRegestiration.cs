b90e81480ccc04e940a08e051368b424dcec68b1
Rashed <Rashed@DESKTOP-KOGV4EJ>
2025-05-15 12:13:08 +0300

    Refactor Program Class

---
+++ Core/Service/ApplicationServicesRegestiration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Service.MappingProfiles;
+using ServiceAbstraction;
+
+namespace Service
+{
+    public static class ApplicationServicesRegestiration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IServiceManager, ServiceManager>();
+            services.AddAutoMapper(typeof(ProductProfile).Assembly);
+            return services;
+        }
+    }
+}
---
+++ Infrastructure/Persistence/InfrastructureRegestiration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Service.MappingProfiles;
+using Service;
+using ServiceAbstraction;
+using Domain.Contracts;
+using Persistence.Repositotories;
+using Persistence.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public static class InfrastructureRegestiration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(connectionString);
+            });
+            services.AddScoped<IDataSeeding, DataSeeding>();
+            services.AddScoped<IUnitOfWork, UnitOfwork>();
+            return services;
+        }
+    }
+}
